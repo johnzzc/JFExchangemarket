92e9dfb603580c3994df54d494e1f0b0dcf7a13c
johnzzc <johnzzc@gmail.com>
2014-05-07 16:36:47 +0800

    0507

--- ExchangeMarket/src/com/jon/exchangemarket/controller/GeneralController.java
+++ ExchangeMarket/src/com/jon/exchangemarket/controller/GeneralController.java
@@ -8,7 +8,7 @@ public class GeneralController extends Controller {
 	@Before(LoginInterceptor.class)
 	public void index() {
 		render("index.html");
-		//sync laptop2
+		//sync laptop0507
 	}
 	
 	public void secondFunction(){
